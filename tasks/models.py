3c8c4e19feb44df1ed4d0cf5fb0e8bb4d2ff59a7
Maiatskaia Natalia <mia@pop-os.localdomain>
2023-04-14 03:38:53 +0600

    Added buttons for editting and deleting

--- tasks/models.py
+++ tasks/models.py
@@ -1,26 +1,10 @@
 from django.db import models
-from django.utils import timezone
 
-
-class TaskItem(models.Model):
-    DEFAULT = 'DEFAULT'
-    PERSONAL  =  'PERSONAL'
-    SHOPPING = 'SHOPPING'
-    WISHLIST = 'WISHLIST'
-    WORK =' WORK'
-    CATEGORIES = (
-        (DEFAULT,DEFAULT),
-        (PERSONAL,PERSONAL),
-        (SHOPPING,SHOPPING),
-        (WISHLIST,WISHLIST),
-        (WORK,WORK)
-    )
-    title = models.CharField(max_length=100,blank=True,null=True)
-    body =models.TextField(null= True,blank=True)
-    due_date = models.DateField(default= timezone.now)
-    task_finished = models.BooleanField(default= True)
-    category = models. CharField(max_length= 20, choices= CATEGORIES , default= DEFAULT)
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50, default="", blank=True)
+    description = models.CharField(max_length=200,  default="", blank=True)
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.title}'
-
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,28 +1,54 @@
-from django.shortcuts import redirect, render
-from django.views.generic import ListView,CreateView, UpdateView,DeleteView
-from .models import TaskItem
-from .forms import TaskItemCreateForm,TaskItemUpdateForm
+from django.shortcuts import render, redirect
+from .models import Task
 
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        t = request.POST.get('title')
+        desc = request.POST.get('description')
+        isComp = request.POST.get('isComplete')
+        if len(t)<1 or len(desc)<1:
+            return redirect('/')
 
-class TodoItemListView(ListView):
-    model = TaskItem
-    template_name = 'tasks/todoitem_list.html'
+        if isComp == 'on':
+            isComp = True
+        else:
+            isComp = False
 
-class TodoItemCreateView(CreateView):
-    model  = TaskItem
-    template_name ='tasks/todoitemcreate_form.html'
-    form_class  =TaskItemCreateForm
-    success_url = '/todo/list'
-    
+        task = Task(title=t, description=desc, is_done=isComp)
+        task.save()
 
-class TodoItemUpdateView(UpdateView):
-    model  = TaskItem
-    template_name ='tasks/todoitem_update_form.html'
-    form_class  =TaskItemUpdateForm
-    success_url = '/todo/list'
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'index.html', context)
 
+def edittask(request, pk):
+    if request.method == "POST":
+        newtitle = request.POST.get('title')
+        newdesc = request.POST.get('description')
+        newstatus = request.POST.get('isComplete')
+        task = Task.objects.get(id=pk)
+        if len(newtitle)<1 or len(newdesc)<1:
+            return redirect('/')
 
-class TodoItemDeleteView(DeleteView):
-    model = TaskItem
-    template_name = 'tasks/todoitem_delete_form.html'
-    success_url = "/todo/list"
+        if newstatus == 'on':
+            newstatus = True
+        else:
+            newstatus = False
+        task.title = newtitle
+        task.description = newdesc
+        task.is_done = newstatus
+        task.save()
+        return redirect('/')
+
+    task = Task.objects.get(id=pk)
+    context = {'title': task.title, 'description': task.description, 'status': task.is_done, "id": task.id}
+    return render(request, 'edit.html', context)
+
+def delete(request, pk):
+    if request.method == "POST":
+        task = Task.objects.get(id = pk)
+        task.delete()
+        return redirect('/')
+    task = {"task": Task.objects.get(id = pk)}
+    return render(request, 'delete.html', task)
